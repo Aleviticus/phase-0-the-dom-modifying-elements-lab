7ae026a8e6236b9aec7e10b6586ffe25387f9432
Aaron Levy <aaronlevypc21@gmail.com>
2023-12-13 14:08:55 -0500

    Complete Lab

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ mainElement.remove();
 const newHeader = document.createElement("h1");
 newHeader.setAttribute("id", "victory");
 newHeader.textContent = "YOUR-NAME is the champion";
+document.body.append(element);
+const ul = document.createElement("ul");
+for (let i = 0; i < 3; i++) {
+    const li = document.createElement("li");
+    li.textContent = (i + 1).toString();
+    ul.append(li);
+  }
+  element.append(ul);
